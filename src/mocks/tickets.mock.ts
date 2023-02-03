3b7a6337b82f8d4ca24e75e65805a4accf1cfa4e
he-lau <he.laurent.1130@gmail.com>
2023-02-03 19:24:46 +0100

    Exercice 1

--- src/mocks/tickets.mock.ts
+++ src/mocks/tickets.mock.ts
@@ -7,12 +7,14 @@ export const TICKETS_MOCKED: Ticket[] = [
     title: 'SI4 in Exercice 0',
     description: '',
     date: dateToday,
-    student: 'Paul'
+    student: 'Paul',
+    major: 'SI',
   },
   {
     title: 'SI5 in Paris',
     description: 'Description du voyage',
     date: dateToday,
-    student: 'Anakin'
+    student: 'Anakin',
+    major: 'GE',
   },
 ];
